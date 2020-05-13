aa9838b2b228b7ea399ccbc70263f02dd2445c0f
Jervis Bay <jervis.bay@gmail.com>
2020-05-12 21:53:37 -0400

    links all working, very very MVP

--- public/assets/js/workouts.js
+++ public/assets/js/workouts.js
@@ -1,17 +1,23 @@
-function getWorkoutDuration() { 
-    var workoutDuration = parseInt($('input[name="workoutDuration"]:checked').serializeArray()[0].value);
-    console.log("Workout Duration: " + workoutDuration);
-    alert("Workout Duration: " + workoutDuration);
-} 
-
-function getMuscleGroups() {
-var muscleGroups = $('input[name="muscleGroup"]:checked').serializeArray().map(ele => ele.value);
-console.log(muscleGroups);
-alert("Muscle Groups to BEAST: " + muscleGroups);
+var workoutParameters = {
+   
+    getWorkoutDuration: function(cb) { 
+        var workoutDuration = parseInt($('input[name="workoutDuration"]:checked').serializeArray()[0].value);
+        console.log("Workout Duration: " + workoutDuration);
+        alert("Workout Duration: " + workoutDuration);
+        cb(workoutDuration);
+    }, 
+    getMuscleGroups: function(cb) {
+        var muscleGroups = $('input[name="muscleGroup"]:checked').serializeArray().map(ele => ele.value);
+        console.log(muscleGroups);
+        alert("Muscle Groups to BEAST: " + muscleGroups);
+        cb(muscleGroups);
+    }    
 }
 
-$("#submitWorkoutParameters").on("click", function(event) {
-    console.log("FIRE UP!");
-getWorkoutDuration();
-getMuscleGroups();
-})
+// $("#submitWorkoutParameters").on("click", function(event) {
+//     console.log("FIRE UP!");
+//     getWorkoutDuration();
+//     getMuscleGroups();
+// })
+
+// module.exports = workoutParameters;
