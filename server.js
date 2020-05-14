a3017f0d2374cb0f3be1bfec173e67defdf9b7f5
Brus Palaj <brusp@bruss-mbp.home>
2020-05-14 12:43:33 -0400

    heroku staging stuff

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var db = require("./models"); 
 
 var PORT = process.env.PORT || 8080;
 
@@ -24,8 +25,11 @@ var routes = require("./controllers/workouts_controller.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-});
+db.sequelize.sync().then(() => {
+
+    app.listen(PORT, function() {
+        // Log (server-side) when our server has started
+        console.log("Server listening on: http://localhost:" + PORT);
+    });
+
+})
